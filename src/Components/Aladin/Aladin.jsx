c4a612abb2ab4f711104c33f9171e943577f7286
MrVtR <30987772+MrVtR@users.noreply.github.com>
2022-10-02 21:38:47 -0300

    Made some adjusts on the visualizer and included captions on it

--- src/Components/Aladin/Aladin.jsx
+++ src/Components/Aladin/Aladin.jsx
@@ -4,57 +4,44 @@ const Aladin = () => {
   useEffect(() => {
     let aladin = window.A.aladin('#aladin-lite-div', {
       survey: 'P/DSS2/color',
-      fov: 1,
-      target: 'M81',
+      target: '303.2763190 +40.2049045',
       showSimbadPointerControl: true,
       showShareControl: true,
-    });
-    aladin.setFov(1);
-    // define function triggered when  a source is hovered
-    aladin.on('objectHovered', function (object) {
-      var msg;
-      if (object) {
-        msg =
-          'You hovered object ' +
-          object.data.name +
-          ' located at ' +
-          object.ra +
-          ', ' +
-          object.dec;
-      } else {
-        msg = 'No object hovered';
-      }
-      $('#infoDiv').html(msg);
+      showLayersControl: false,
     });
 
-    // define function triggered when an object is clicked
-    var objClicked;
-    aladin.on('objectClicked', function (object) {
-      var msg;
-      if (object) {
-        objClicked = object;
-        object.select();
-        msg =
-          'You clicked object ' +
-          object.data.name +
-          ' located at ' +
-          object.ra +
-          ', ' +
-          object.dec;
-      } else {
-        objClicked.deselect();
-        msg = 'You clicked in void';
-      }
-      $('#infoDiv').html(msg);
-    });
+    var cat = window.A.catalog({ name: 'Some markers', sourceSize: 28 });
+    aladin.addCatalog(cat);
+    cat.addSources([
+      window.A.marker(294.1752292, +42.1185194, {
+        popupTitle: 'KIC 6922244',
+        popupDesc:
+          '<em>Main ID:</em>Kepler-8<br/><em>Object Type:</em> Rotating Variable<br/><em>Spectral Type:</em> F6V<br/><em>Right Ascension:</em> 18 45 09.1489991376<br/><em>Declination:</em> +42 27 03.891327156<br/><br/>You can check for more info <a target="_blank" href="http://simbad.u-strasbg.fr/simbad/sim-id?Ident=KIC%209107656">in Simbad</a>',
+      }),
+    ]);
+    cat.addSources([
+      window.A.marker(297.6950402, +45.4700724, {
+        popupTitle: 'KIC 9107656',
+        popupDesc:
+          '<em>Main ID:</em>2MASS J19504680+4528121<br/><em>Object Type:</em> Rotating Variable<br/><em>Right Ascension:</em> 19 50 46.8096471960<br/><em>Declination:</em> +45 28 12.260607096<br/><br/>You can check for more info <a target="_blank" href="http://simbad.u-strasbg.fr/simbad/sim-id?Ident=KIC%209107656">in Simbad</a>',
+      }),
+    ]);
+    cat.addSources([
+      window.A.marker(286.1507476, +37.379088, {
+        popupTitle: 'KIC 1849238',
+        popupDesc:
+          '<em>Main ID:</em>2MASS J19043618+372244<br/><em>Object Type:</em> Rotating Variable<br/><em>Right Ascension:</em> 19 04 36.1794288576<br/><em>Declination:</em> +37 22 44.716910556<br/><br/>You can check for more info <a target="_blank" href="http://simbad.u-strasbg.fr/simbad/sim-id?Ident=KIC%201849238">in Simbad</a>',
+      }),
+    ]);
   }, []);
-
-  console.log(window.A.catalogFromSimbad('M 81'));
 
   return (
     <div
       id="aladin-lite-div"
-      style={{ height: '80vh', marginLeft: '20px', marginRight: '20px' }}
+      style={{
+        width: '80vw',
+        height: '80vh',
+      }}
     />
   );
 };
--- src/Pages/Visualizer/Visualizer.jsx
+++ src/Pages/Visualizer/Visualizer.jsx
@@ -1,10 +1,79 @@
 import { Navbar, Aladin } from '../../Components/exportComponents';
-
+import './Visualizer.scss';
+import f1 from '../../Assets/images/f1.png';
+import f2 from '../../Assets/images/f2.png';
+import f3 from '../../Assets/images/f3.png';
+import f4 from '../../Assets/images/f4.png';
 export default function Visualizer() {
   return (
     <div className="visualizer">
       <Navbar />
-      <Aladin />
+      <div className="aladin">
+        <ul className="legenda">
+          Visualizer Functions:
+          <li>Search Object</li>
+          <ul>
+            <li className="sub-item">
+              <img src={f1} alt="" />
+              You can Search objects by it's name or celestial coordinates
+            </li>
+          </ul>
+          <li>Point to Object</li>
+          <ul>
+            <li className="sub-item">
+              <img src={f2} alt="" />
+              You can point to any kind of object on the visualizer to obtain
+              more details about it
+            </li>
+          </ul>
+          <li>Zoom in/out</li>
+          <ul>
+            <li className="sub-item">
+              <img src={f3} alt="" />
+              You can zoom in/out the visualizer with the mouse scroll and the
+              buttons +/-
+            </li>
+          </ul>
+          <li>Create share link</li>
+          <ul>
+            <li className="sub-item">
+              <img src={f4} alt="" />
+              It's possible to share your current view with this button
+            </li>
+          </ul>
+        </ul>
+        <Aladin />
+        <ul className="legenda">
+          Caption:
+          <li>Main ID:</li>
+          <ul>
+            <li className="sub-item">Object Identification</li>
+          </ul>
+          <li>Object Type:</li>
+          <ul>
+            <li className="sub-item">The Object main classification</li>
+          </ul>
+          <li>Spectral Type:</li>
+          <ul>
+            <li className="sub-item">
+              Star Classification based on the relative strengths of their
+              absorption lines
+            </li>
+          </ul>
+          <li>Right Ascension:</li>
+          <ul>
+            <li className="sub-item">
+              Celestial sphere's equivalent of longitude
+            </li>
+          </ul>
+          <li>Declination:</li>
+          <ul>
+            <li className="sub-item">
+              Celestial sphere's equivalent of latitude
+            </li>
+          </ul>
+        </ul>
+      </div>
     </div>
   );
 }
